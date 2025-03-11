f018fa548c554ea1d242ac69405190b1c06eddaf
Khurram Nadir <128932056+KdeDeveloper@users.noreply.github.com>
2025-03-11 18:00:08 +0500

    Add Kubernetes manifests and Flask application for deployment

---
+++ k8s-flask-app/down.bat
@@ -0,0 +1,8 @@
+kubectl delete -f manifests/deployment/flask-deployment.yaml
+kubectl delete -f manifests/services/flask-service.yaml
+
+kubectl delete -f manifests/deployment/postgres-deployment.yaml
+kubectl delete -f manifests/services/postgres-service.yaml
+
+kubectl delete -f manifests/configmap/
+kubectl delete -f manifests/secret/
---
+++ k8s-flask-app/up.bat
@@ -0,0 +1,6 @@
+kubectl apply -f manifests/configmap/
+kubectl apply -f manifests/secret/
+kubectl apply -f manifests/deployment/postgres-deployment.yaml
+kubectl apply -f manifests/services/postgres-service.yaml
+kubectl apply -f manifests/deployment/flask-deployment.yaml
+kubectl apply -f manifests/services/flask-service.yaml
